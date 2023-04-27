77a028f05a61b548c549dbb6da14b398c8f422e2
martimayoral <76410086+martimayoral@users.noreply.github.com>
2023-04-27 13:01:54 +0200

    logs update

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,3 +1,17 @@
+interface Log {
+    date: string;
+    errorCode: string;
+    errorMessage: string;
+    user: string;
+}
+
 export function Logs() {
+    const logs: Log[] = [
+        { date: "10-20-2022", errorCode: "500", errorMessage: "bla bla", user: "pepito" },
+        { date: "15-22-2023", errorCode: "500", errorMessage: "bla bla", user: "pepita" },
+        { date: "12-43-2022", errorCode: "500", errorMessage: "blu bli", user: "josue" }
+    ]
+
+    // fer
     return <div>rellenar aqui</div>
 }
