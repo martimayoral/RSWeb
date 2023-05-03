9624399f924925e9d45a381561f1f74aab9e1d0c
martimayoral <76410086+martimayoral@users.noreply.github.com>
2023-05-03 18:26:13 +0200

    logs

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,11 +1,10 @@
+import { Box } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Log {
-    date: string;
-    errorCode: string;
-    errorMessage: string;
-    user: string;
+    date: string
+    errorCode: string
 }
 
 export function Logs() {
@@ -13,23 +12,38 @@ export function Logs() {
     useEffect(() => {
         axios.get("/getLogs")
             .then(response => {
-                console.log(response.data)
+                console.log("logs: ", response.data)
 
                 // transformar response.data a un array de Logs
                 // i posarlos aqui amb setLogs
-                const newLogs: Log[] = []
+                const newLogs: Log[] =
+                    response.data.map((report: any) => {
+                        const log: Log = {
+                            date: report.dateError,
+                            errorCode: report.error
+                        }
+                        return log
+                    })
 
                 setLogs(newLogs)
             })
 
     }, [])
-
-    const logsFake: Log[] = [
-        { date: "10-20-2022", errorCode: "500", errorMessage: "bla bla", user: "pepito" },
-        { date: "15-22-2023", errorCode: "500", errorMessage: "bla bla", user: "pepita" },
-        { date: "12-43-2022", errorCode: "500", errorMessage: "blu bli", user: "josue" }
-    ]
+    /* 
+        const logsFake: Log[] = [
+            { date: "10-20-2022", errorCode: "500", errorMessage: "bla bla", user: "pepito" },
+            { date: "15-22-2023", errorCode: "500", errorMessage: "bla bla", user: "pepita" },
+            { date: "12-43-2022", errorCode: "500", errorMessage: "blu bli", user: "josue" }
+        ] */
 
     // fer
-    return <div>rellenar aqui {logs.map((log) => log.user + ".")}</div>
+    return <div>rellenar aqui
+        {logs.map((log, i) => (
+            <Box key={i}>
+                {log.date} marina {i}
+                <br />
+                <br />
+            </Box>
+        ))}
+    </div >
 }
